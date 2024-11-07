7be45d3032b789d24b39d4ab9d4af12140a14eb4
omd34 <133078442+omd34@users.noreply.github.com>
2024-11-07 07:21:46 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
-
-(function() {
-  "use strict";
-
-  /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
-  function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-  }
-
+document.addEventListener("DOMContentLoaded", function() {
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
 
@@ -134,4 +121,9 @@
    */
   new PureCounter();
 
+})();();
+
 })();
+
+
+});
